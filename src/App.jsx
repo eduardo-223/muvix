00a96872a3bb37e3137622a0d02dd03aff861948
Eduardo Ranel <eduardo_rp23@hotmail.com>
2023-08-10 23:30:44 -0300

    feature: created first paste of projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { fetchDataFromApi } from "./utils/api";
-import {getApiConfiguration} from "./store/homeSlice"
+import {getApiConfiguration} from "./store/homeSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import PageNotFound from "./pages/404/PageNotFound";
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
 
 function App() {
   const dispatch = useDispatch();
@@ -18,7 +25,8 @@ function App() {
     });
   };
 
-  return <div className="App">App {url?.total_pages}</div>;
+  return (<BrowserRouter>
+  </BrowserRouter>)
 }
 
 export default App;
