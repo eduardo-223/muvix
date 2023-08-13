9fa586ff6f896ca878dfcf2f60ec60ddc01be53f
Eduardo Ranel <eduardo_rp23@hotmail.com>
2023-08-12 23:27:10 -0300

    Fix: fixing carousel not working error for desktop

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import {
-    BsFillArrowLeftCircleFill,
-    BsFillArrowRightCircleFill,
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
 } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -9,70 +9,91 @@ import dayjs from "dayjs";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
-import CircleRating from './../circleRating/CircleRating';
+import CircleRating from "./../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 import "./style.scss";
 
-function Carousel({data, loading}) {
-    const carouselContainer = useRef();
-    const {url} = useSelector((state)=> state.home);
-    const navigate = useNavigate();
+function Carousel({ data, loading }) {
+  const carouselContainer = useRef();
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
 
-    const navigation = (dir) =>{
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
 
-    }
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
-    const skItem = () =>{
-        <div className="skeletonItem">
-            <div className="posterBlock skeleton"></div>
-            <div className="textBlock">
-                <div className="title skeleton"></div>
-                <div className="date skeleton"></div>
-            </div>
-        </div>
-    }
-    
+  const skItem = () => {
+    <div className="skeletonItem">
+      <div className="posterBlock skeleton"></div>
+      <div className="textBlock">
+        <div className="title skeleton"></div>
+        <div className="date skeleton"></div>
+      </div>
+    </div>;
+  };
+
   return (
     <div className="carousel">
-        <ContentWrapper>
-            <BsFillArrowLeftCircleFill className="carouselLeftNav arrow" onClick={()=>navigation("left")}/>
-            <BsFillArrowRightCircleFill className="carouselRighttNav arrow" onClick={()=>navigation("right")}/>
-            {!loading ? (
-                <div className="carouselItems">
-                    {data?.map((item) =>{
-                        const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback;
-                        
-                        return (
-                            <div key={item.id} className="carouselItem">
-                                <div className="posterBlock">
-                                    <Img src={posterUrl} alt="" />
-                                    <CircleRating rating={item.vote_average.toFixed(1)}/>
-                                    <Genres data={item.genre_ids.slice(0, 2)}/>
-                                </div>
-                                <div className="textBlock">
-                                    <span className="title" lang="pt">
-                                        {item.title || item.name}
-                                    </span>
-                                    <span className="date">
-                                        {dayjs(item.release_Date).format("D MMM, YYYY")}
-                                    </span>
-                                </div>
-                            </div>
-                        )
-                    })}
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRighttNav arrow"
+          onClick={() => navigation("right")}
+        />
+        {!loading ? (
+          <div className="carouselItems" ref={carouselContainer}>
+            {data?.map((item) => {
+              const posterUrl = item.poster_path
+                ? url.poster + item.poster_path
+                : PosterFallback;
+
+              return (
+                <div
+                  key={item.id}
+                  className="carouselItem"
+                  onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                >
+                  <div className="posterBlock">
+                    <Img src={posterUrl} alt="" />
+                    <CircleRating rating={item.vote_average.toFixed(1)} />
+                    <Genres data={item.genre_ids.slice(0, 2)} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title" lang="pt">
+                      {item.title || item.name}
+                    </span>
+                    <span className="date">
+                      {dayjs(item.release_Date).format("D MMM, YYYY")}
+                    </span>
+                  </div>
                 </div>
-            ):(
-                <div className="loadingSkeleton">
-                    {skItem()}
-                    {skItem()}
-                    {skItem()}
-                    {skItem()}
-                    {skItem()}
-                </div>
-            )}
-        </ContentWrapper>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="loadingSkeleton">
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+          </div>
+        )}
+      </ContentWrapper>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
